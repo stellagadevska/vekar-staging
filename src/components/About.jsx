968f74d03dd0fb88a1327a22a78ca273f520ffd0
Stella Gadevska <58764584+stellagadevska@users.noreply.github.com>
2024-12-24 12:02:12 +0200

    CleanUp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,6 @@ import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 import SectionWrapper from "../hoc/SectionWrapper";
@@ -71,12 +70,6 @@ const About = () => {
           </motion.button>
         </Link>
       </div>
-
-      {/* <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div> */}
     </>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,102 +1,4 @@
-import {
-  mobile,
-  backend,
-  creator,
-  web,
-  javascript,
-  typescript,
-  html,
-  css,
-  reactjs,
-  redux,
-  tailwind,
-  nodejs,
-  mongodb,
-  git,
-  figma,
-  docker,
-  yin,
-  yang,
-  yinyang,
-  carrent,
-  jobit,
-  tripguide,
-  threejs,
-} from "../assets";
-
-const services = [
-  {
-    title: "Web Developer",
-    icon: web,
-  },
-  {
-    title: "React.js Developer",
-    icon: mobile,
-  },
-  {
-    title: "FrontEnd Developer",
-    icon: backend,
-  },
-  {
-    title: "Content Creator",
-    icon: creator,
-  },
-];
-
-const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
-  {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
-    name: "git",
-    icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
-  },
-];
+import { yin, yang, yinyang } from "../assets";
 
 const experiences = [
   {
@@ -104,175 +6,64 @@ const experiences = [
     company_name: "Sustain",
     icon: yang,
     iconBg: "#000000",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Branding",
     company_name: "Create",
     icon: yin,
     iconBg: "#FFFFFF",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Company Registration",
     company_name: "Sustain",
     icon: yang,
     iconBg: "#000000",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Accounting and Taxation",
     company_name: "Create",
     icon: yin,
     iconBg: "#FFFFFF",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Market Search",
     company_name: "Sustain",
     icon: yang,
     iconBg: "#000000",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Brand Psychology",
     company_name: "Create",
     icon: yin,
     iconBg: "#FFFFFF",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Advertising and Media Present",
     company_name: "Sustain",
     icon: yang,
     iconBg: "#000000",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Graphic Design and UGC Content Creation",
     company_name: "Create",
     icon: yin,
     iconBg: "#FFFFFF",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
   {
     title: "Software Development and Business Automation",
     company_name: "Sustain and Create",
     icon: yinyang,
     iconBg: "#7D7D7D",
-    points: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    ],
+    points: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit."],
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
-
-const projects = [
-  {
-    name: "Car Rent",
-    description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: carrent,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Job IT",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
-  },
-];
-
-export { services, technologies, experiences, testimonials, projects };
+export { experiences };
